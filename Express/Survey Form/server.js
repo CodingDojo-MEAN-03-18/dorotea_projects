4b725b1261c5dfebcce190c60c41927e438992e7
Teya <dlechuga30@gmail.com>
2018-03-29 18:40:27 -0400

    updated forms logic

--- Express/Survey Form/server.js
+++ Express/Survey Form/server.js
@@ -22,22 +22,22 @@ app.set('view engine', 'ejs');
 //create variable to hold user information
 var user = {};
 
-app.get('/', function(req,res){
-  res.render('index');
+app.get('/', function (req, res) {
+    res.render('index');
 })
 
-app.post('/submit', function(req,res){
-  user['name'] = req.body['name'];
-  user['location'] = req.body['location'];
-  user['language'] = req.body['language'];
-  user['comment'] = req.body['comment'];
-  res.redirect("/result");
+app.post('/submit', function (req, res) {
+    user['name'] = req.body['name'];
+    user['location'] = req.body['location'];
+    user['language'] = req.body['language'];
+    user['comment'] = req.body['comment'];
+    res.redirect('/result');
 })
 
-app.get('/result', function(req,res){
-  res.render("result", {user:user});
+app.get('/result', function (req, res) {
+    res.render("result", {user: user});
 })
 
-app.listen(8000, function(){
-  console.log('Listening on port 8000');
+app.listen(8000, function () {
+    console.log('Listening on port 8000');
 })
