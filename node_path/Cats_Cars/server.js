678611f98e8913a1a02e69f0cc3350c04351df56
Teya <dlechuga30@gmail.com>
2018-04-01 18:43:15 -0400

    finished Cats and Cars Assignment, now will render images and css

--- node_path/Cats_Cars/server.js
+++ node_path/Cats_Cars/server.js
@@ -8,22 +8,14 @@
 var express = require("express");
 var path = require("path");
 
-//tell where to look for files
-app.use(express.static(path.join(__dirname, "./static")))
-app.set('views', path.join(__dirname, './views'));
-app.set('images', path.join(__dirname, './images'));
-app.set('css', path.join(__dirname, './css'));
+var app = express();
+
+//this tells where to find the views/html files!
 app.set('view engine', 'ejs');
+//this telss express for any static files like css or images, look in the public folder!
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-  //put styel sheet reding in a function that cna call
-  //else if(request.url === '/stylesheets/style.css'){
-  //  fs.readFile('./stylesheets/style.css', 'utf8', function(errors, contents){
-  //   response.writeHead(200, {'Content-type': 'text/css'});
-  //   response.write(contents);
-  //   response.end();
-//    })
-//  }
     res.render('index');
 })
 
@@ -35,7 +27,7 @@ app.get('/cats', function (req, res) {
     res.render('cats');
 })
 
-app.get('/cars/news', function (req, res) {
+app.get('/cars/new', function (req, res) {
     res.render('carnew');
 })
 
