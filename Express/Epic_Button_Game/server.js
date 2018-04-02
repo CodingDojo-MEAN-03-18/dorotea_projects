53113d4c0017bb721d722b24e2ddf09d39a7c6f7
Teya <dlechuga30@gmail.com>
2018-04-01 23:01:13 -0400

    Began Epic Button Game Assignment

--- Express/Epic_Button_Game/server.js
+++ Express/Epic_Button_Game/server.js
@@ -2,19 +2,26 @@
 //npm install socket.io
 //npm install body-parser
 
+//left off at 17:00 on lecture
+
 const express = require('express');
-const path= require('path');
-const port = process.env.port || 8000;
+const path = require('path');
+const port = process.env.PORT || 8000;
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'client')));
 
-const server = app.listen(8000, function () {
-    console.log('Listening on port 8000');
-})
-
+const server = app.listen(port, () => console.log('listening on port' + port));
 const io = require('socket.io')(server);
+//var io = require('socket.io')(server);
 
 io.on('connection', socket => {
+
+  let count = 0
+
   console.log('Hello World');
+
+  socket.on('buttonClicked', function(){
+    io.emit('numberUpdated', ++count);
+  });
 });
