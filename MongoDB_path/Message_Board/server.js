1f0ab942803eb315c2127cc5d44dd9ef96afe3b9
Teya <dlechuga30@gmail.com>
2018-04-08 08:10:43 -0400

    Completed message board assignment

--- MongoDB_path/Message_Board/server.js
+++ MongoDB_path/Message_Board/server.js
@@ -1,42 +1,58 @@
-//npm init-y
-//npm install Express
-//npm install ejs
-//npm install body-parser
-//npm install mongoose
-
-// Require the Express Module
-const express = require('express')
-
-// Create an Express App
+const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const path = require("path");
+const bodyParser = require("body-parser");
 
-// Integrate body-parser with our App
-const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("views", path.join(__dirname, "./views"));
+app.set("view engine", "ejs");
 
-// Require path
-const path = require('path');
 
-//Require mongoose
-const mongoose = require('mongoose');
 
-// Set up body-parser to parse form data
-app.use(bodyParser.urlencoded({extended: false}));
+app.listen(8000, function() {
+	console.log("server running on port 8000");
+});
 
-// Setting our Views Folder Directorycopy
-app.set('views', path.join(__dirname, './views'));
+mongoose.connect('mongodb://127.0.0.1/message_board', function(err, db) {
+	if (err) {
+		console.log("error here");
+		console.log(err);
+	}
+});
 
-// Setting our View Engine set to EJS
-app.set('view engine', 'ejs');
+// Use native promises
+mongoose.Promise = global.Promise;
 
-//Connect Mongoose to MongoDB
-mongoose.connect('mongodb://localhost/Message_Board');
+const Schema = mongoose.Schema;
 
-//route to dispkay index and messsages
-app.get("/", function(req, res) {
-	Message.find({}, false, true).populate('_comments').exec(function(err, messages) {
-	      res.render('index.ejs', { messages: messages });
+const MessageSchema = new mongoose.Schema({
+	name: String,
+	message: String,
+	_comments: [{type: Schema.Types.ObjectId, ref: 'Comment'}]
+},{ timestamps: true });
+
+mongoose.model('Message', MessageSchema);
+const Message = mongoose.model("Message");
+
+
+const CommentSchema = new mongoose.Schema({
+	name: String,
+	text: String,
+	_message: {type: Schema.Types.ObjectId, ref: 'Message'}
+});
+
+
+mongoose.model("Comment", CommentSchema);
+
+const Comment = mongoose.model("Comment");
+
+app.get('/', function(req, res){
+	arr = Message.find({}).populate('_comments').exec(function(err, messages){
+		res.render('index', {m:messages})
 	});
 });
+
 
 app.post("/message", function(req, res){
 	var newMessage = new Message({ name: req.body.name, message: req.body.message });
@@ -50,6 +66,7 @@ app.post("/message", function(req, res){
 		}
 	})
 })
+
 app.post("/comment/:id", function(req, res) {
 	const messageId = req.params.id;
 	Message.findOne({ _id: messageId }, function(err, message) {
@@ -69,37 +86,3 @@ app.post("/comment/:id", function(req, res) {
 		});
 	});
 });
-
-
-//define schema variable
-const Schema = mongoose.Schema;
-
-//define sechmas
-//set up one to many relationship by referencing Shema.Types.ObjectID, ref: comment
-const MessageSchema = new mongoose.Schema({
-	name: String,
-	message: String,
-	_comments: [{type: Schema.Types.ObjectId, ref: 'Comment'}]
-});
-
-//set validation
-MessageSchema.path('name').required(true, 'Name cannot be blank');
-MessageSchema.path('message').required(true, 'Message cannot be blank');
-mongoose.model("Message", MessageSchema);
-
-const Message = mongoose.model("Message");
-const CommentSchema = new mongoose.Schema({
-	name: String,
-	text: String,
-	_message: {type: Schema.Types.ObjectId, ref: 'Message'}
-});
-
-CommentSchema.path('name').required(true, 'Name cannot be blank');
-CommentSchema.path('text').required(true, 'Comment cannot be blank');
-mongoose.model("Comment", CommentSchema);
-
-const Comment = mongoose.model("Comment");
-
-app.listen(8000, function () {
-    console.log("listening on port 8000");
-})
