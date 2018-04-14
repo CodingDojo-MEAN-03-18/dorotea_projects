a9ed2b92802aaa642012aa57acdaf1185ad626f4
Teya <dlechuga30@gmail.com>
2018-04-14 19:49:23 -0400

    completed Retro Barcode Generator Assignment

--- Angular_path/retroBarcodeGen/src/app/app.component.ts
+++ Angular_path/retroBarcodeGen/src/app/app.component.ts
@@ -1,14 +1,36 @@
 import { Component } from '@angular/core';
 
+var potential = [
+  '#1D206B',
+  '#872929',
+  '#A39130',
+  '#40A330',
+  '#455742',
+  '#61B49A',
+  '#0C4936',
+  '#5EBEE0',
+  '#295AA8',
+  '#1F2939',
+  '#BA57B5',
+  '#8B0C18'
+];
+
+var color = []
+for (var i=0; i < 15; i++){
+  color.push(potential[Math.floor(Math.random()*10)])
+};
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 
-var color = (Math.floor(Math.random()*6)) + 1;
+
+
+
 
 export class AppComponent {
   title = 'Coding Dojo Retro Barcode Generator Assignment';
-  
+  colors = color;
 }
