aaf41881313ca9181e895d156297a1422d9410b3
Teya <dlechuga30@gmail.com>
2018-04-14 17:14:22 -0400

    completed types and classes assignment

--- Angular_path/Types and Classes/server.ts
+++ Angular_path/Types and Classes/server.ts
@@ -1,33 +1,30 @@
 //Given the code below, re-write it using all TypeScript features you have learned
 myNum = 5;
-var myNum: number = 5;
+const myNum: number = 5;
 
 
 myString = "Hello Universe";
-var myString: string = "Hello Universe";
+const myString: string = "Hello Universe";
 
 myArr = [1,2,3,4];
-var myArr: number[1,2,3,4];
+const myArr: number[1,2,3,4];
 
 myObj = { name:'Bill'};
-function myObj(){
-    var myObj = 'Bill';
-}
-
+let myObj: { [key: string]: (string | number) } = { name: 'Bill' };
 
 anythingVariable = "Hey";
-var anythingVariable: any = "Hey";
+let anythingVariable: any = "Hey";
 
 
 anythingVariable = 25; 
-var anythingVariable: any = 25;
+let anythingVariable: any = 25;
 
 arrayOne = [true, false, true, true]; 
-var arrayOne: boolean[true, false, true, false];
+var arrayOne: boolean[] = [true, false, true, false];
 
 
 arrayTwo = [1, 'abc', true, 2];
-var arrayTwo: any[1, 'abc', true, 2];
+var arrayTwo: (number | string | boolean)[] = [1, 'abc', true, 2];
 
 
 
@@ -48,12 +45,39 @@ MyNode = (function () {
     };
     return MyNode;
 }());
+
+class MyNode {
+    private _priv: number;
+  
+    constructor(public val: number) {}
+  
+    doSomething(): void {
+      this._priv = 10;
+    }
+  }
+  
+
+
 myNodeInstance = new MyNode(1);
 console.log(myNodeInstance.val);
+
+const myNodeInstance: MyNode = new MyNode(1);
+console.log(myNodeInstance.val);
+
 function myFunction() {
     console.log("Hello World");
     return;
 }
+
+
+function myFunction(): void {
+    console.log('Hello World');
+    return;
+}
 function sendingErrors() {
+	throw new Error('Error message');
+}
+
+function sendingErrors(): never {
 	throw new Error('Error message');
 }
