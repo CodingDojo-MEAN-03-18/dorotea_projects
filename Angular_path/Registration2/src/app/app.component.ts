5f270271254a3947dae21dee6867c85ad1089a0e
Teya <dlechuga30@gmail.com>
2018-04-17 19:52:04 -0400

    Updated Registration Project, need to correct ngif

--- Angular_path/Registration2/src/app/app.component.ts
+++ Angular_path/Registration2/src/app/app.component.ts
@@ -11,19 +11,16 @@ import { NgForm } from '@angular/forms';
 export class AppComponent {
   title = 'Coding Dojo Registration Form Assignment';
 
-  form_submitted: boolean = false;
-  //form_complete: boolean = false;
 
   user: User = new User();
   users: Array<User> = [];
 
   onSubmit(form: NgForm, event: Event)  {
     event.preventDefault();
-    this.form_submitted = true;
     console.log (form.value);
     this.users.push(this.user);
     this.user = new User();
-    //form.reset();
+    form.reset();
   }
 
 }
