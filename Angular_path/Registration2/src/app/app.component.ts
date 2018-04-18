6ebaedad457d9b96307a8a15e91812f079b49934
Teya <dlechuga30@gmail.com>
2018-04-17 21:47:10 -0400

    completed registration assignment

--- Angular_path/Registration2/src/app/app.component.ts
+++ Angular_path/Registration2/src/app/app.component.ts
@@ -11,16 +11,17 @@ import { NgForm } from '@angular/forms';
 export class AppComponent {
   title = 'Coding Dojo Registration Form Assignment';
 
+  visible = false;
 
   user: User = new User();
   users: Array<User> = [];
 
-  onSubmit(form: NgForm, event: Event)  {
+  onSubmit(event: Event, form: NgForm)  {
     event.preventDefault();
-    console.log (form.value);
+    console.log (this.user);
     this.users.push(this.user);
+    this.visible = !this.visible;
     this.user = new User();
-    form.reset();
   }
 
 }
